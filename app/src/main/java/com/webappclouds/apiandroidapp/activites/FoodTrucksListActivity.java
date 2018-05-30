cc4fe82a59ad7080168c0e9c790d9bdc0675b971
LAPTOP-92I921B8\Salon Clouds Plus <Daniel@webappclouds.com>
2018-05-30 13:31:15 -0400

    Array Request

--- app/src/main/java/com/webappclouds/apiandroidapp/activites/FoodTrucksListActivity.java
+++ app/src/main/java/com/webappclouds/apiandroidapp/activites/FoodTrucksListActivity.java
@@ -2,11 +2,13 @@ package com.webappclouds.apiandroidapp.activites;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
 import com.webappclouds.apiandroidapp.R;
 import com.webappclouds.apiandroidapp.model.FoodTruck;
 
@@ -27,14 +29,16 @@ public class FoodTrucksListActivity extends AppCompatActivity {
         final JsonArrayRequest getTrucks = new JsonArrayRequest(Request.Method.GET, url, null, new Response.Listener<JSONArray>() {
             @Override
             public void onResponse(JSONArray response) {
+                System.out.println(response.toString());
 
             }
         }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-
+                Log.v("API", "Error: " + error.getLocalizedMessage());
             }
         });
 
+        Volley.newRequestQueue(this).add(getTrucks);
     }
 }
