78b10f0deb5ab9243be917d59bfca8d0c702a445
LAPTOP-92I921B8\Salon Clouds Plus <Daniel@webappclouds.com>
2018-05-30 16:39:57 -0400

    Food Truck Detail View

--- app/src/main/java/com/webappclouds/apiandroidapp/activites/FoodTruckDetailActivity.java
+++ app/src/main/java/com/webappclouds/apiandroidapp/activites/FoodTruckDetailActivity.java
@@ -2,6 +2,7 @@ package com.webappclouds.apiandroidapp.activites;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -10,31 +11,32 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.webappclouds.apiandroidapp.R;
+import com.webappclouds.apiandroidapp.model.FoodTruck;
 
 public class FoodTruckDetailActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private FoodTruck foodTruck;
+    private TextView truckName;
+    private TextView foodType;
+    private TextView avgCost;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_food_truck_detail);
+        setContentView(R.layout.detail_view);
+
+        truckName = findViewById(R.id.detail_truck_name);
+        foodType = findViewById(R.id.detail_food_type);
+        avgCost = findViewById(R.id.detail_food_cost);
+
+        foodTruck = getIntent().getParcelableExtra(FoodTrucksListActivity.EXTRA_ITEM_TRUCK);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
 
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
@@ -43,5 +45,11 @@ public class FoodTruckDetailActivity extends FragmentActivity implements OnMapRe
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
+    private void updateUI() {
+        truckName.setText(foodTruck.getName());
+        foodType.setText(foodTruck.getFoodType());
+        avgCost.setText("$" + foodTruck.getAvgCost());
     }
 }
