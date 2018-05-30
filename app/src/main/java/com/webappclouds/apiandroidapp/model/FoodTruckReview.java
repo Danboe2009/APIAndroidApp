a65305e1552a1a3936180bb87f47fefcdc1b883b
LAPTOP-92I921B8\Salon Clouds Plus <Daniel@webappclouds.com>
2018-05-30 17:19:13 -0400

    Food Truck Reviews

---
+++ app/src/main/java/com/webappclouds/apiandroidapp/model/FoodTruckReview.java
@@ -0,0 +1,26 @@
+package com.webappclouds.apiandroidapp.model;
+
+public class FoodTruckReview {
+
+    private String id = "";
+    private String title = "";
+    private String text = "";
+
+    public FoodTruckReview(String id, String title, String text) {
+        this.id = id;
+        this.title = title;
+        this.text = text;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getText() {
+        return text;
+    }
+}
