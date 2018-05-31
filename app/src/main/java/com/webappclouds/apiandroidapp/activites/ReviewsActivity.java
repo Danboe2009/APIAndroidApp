1bde7e5c483878dd7ec38df12b2117374fec5fe2
Danboe2009 <Danboe2009@gmail.com>
2018-05-31 10:53:19 -0400

    Retrieve FoodTruck Reviews

--- app/src/main/java/com/webappclouds/apiandroidapp/activites/ReviewsActivity.java
+++ app/src/main/java/com/webappclouds/apiandroidapp/activites/ReviewsActivity.java
@@ -4,14 +4,18 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import com.webappclouds.apiandroidapp.R;
+import com.webappclouds.apiandroidapp.model.FoodTruck;
 
 public class ReviewsActivity extends AppCompatActivity {
+
+    private FoodTruck foodTruck;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reviews);
 
-
+        foodTruck = getIntent().getParcelableExtra(FoodTrucksListActivity.EXTRA_ITEM_TRUCK);
+        System.out.println(foodTruck.getName());
     }
 }
