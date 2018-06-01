5e88bd04c417ffcff9410fab0cf5ea8850135974
Danboe2009 <Danboe2009@gmail.com>
2018-05-31 22:17:41 -0400

    Add Food Truck Review

--- app/src/main/java/com/webappclouds/apiandroidapp/activites/AddReviewActivity.java
+++ app/src/main/java/com/webappclouds/apiandroidapp/activites/AddReviewActivity.java
@@ -3,6 +3,8 @@ package com.webappclouds.apiandroidapp.activites;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.webappclouds.apiandroidapp.R;
+
 public class AddReviewActivity extends AppCompatActivity {
 
     @Override
